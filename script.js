22d211257998d8d208c6fb73a3be62c508fe7241
Swetha-devasenapathy <swethadev002@gmail.com>
2024-04-29 23:07:01 +0530

    Push

---
+++ script.js
@@ -0,0 +1,57 @@
+var form = document.getElementById("myForm"),
+    imgInput = document.querySelector(".img"),
+    file = document.getElementById("imgInput"),
+    userName = document.getElementById("name"),
+    city = document.getElementById("city"),
+    email = document.getElementById("email"),
+    phone = document.getElementById("phone"),
+    sDate = document.getElementById("sDate"),
+    submitBtn = document.querySelector(".submit"),
+    userInfo = document.getElementById("data"),
+    modal = document.getElementById("userForm"),
+    modalTitle = document.querySelector("#userForm .modal-title"),
+    newUserBtn = document.querySelector(".newUser")
+
+
+let getData = localStorage.getItem('userProfile') ? JSON.parse(localStorage.getItem('userProfile')) : []
+
+let isEdit = false, editId
+showInfo()
+
+newUserBtn.addEventListener('click', ()=> {
+    submitBtn.innerText = 'Submit',
+    modalTitle.innerText = "Fill the Details"
+    isEdit = false
+    imgInput.src = "./image/Profile Icon.webp"
+    form.reset()
+})
+
+
+file.onchange = function(){
+    if(file.files[0].size < 1000000){  // 1MB = 1000000
+        var fileReader = new FileReader();
+
+        fileReader.onload = function(e){
+            imgUrl = e.target.result
+            imgInput.src = imgUrl
+        }
+
+        fileReader.readAsDataURL(file.files[0])
+    }
+    else{
+        alert("This file is too large!")
+    }
+}
+
+
+function showInfo(){
+    document.querySelectorAll('.employeeDetails').forEach(info => info.remove())
+    getData.forEach((element, index) => {
+        let createElement = `<tr class="employeeDetails">
+            <td>${index+1}</td>
+            <td><img src="${element.picture}" alt="" width="50" height="50"></td>
+            <td>${element.employeeName}</td>
+            <td>${element.employeeCity}</td>
+            <td>${element.employeeEmail}</td>
+            <td>${element.employeePhone}</td>
+            <td>${element.startDate}</td>
